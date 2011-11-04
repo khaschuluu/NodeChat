a6fa2e60f0d6ccaa497c91392f71eaadf4c5c560
Khaschuluu Munkhbayar <khaschuluu.m@cos.mn>
2011-11-05 00:27:48 +0800

    Анхны коммит. Маргаашийг хүлээж тэсэхгүй нь ээ, учир нь маргааш МБГ 11.11 болно, би үүгээр сэдэв тавих болно :D

---
+++ app.js
@@ -0,0 +1,192 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+
+var app = module.exports = express.createServer();
+
+var io = require('socket.io').listen(app);
+
+var UserModel = require('./user.js').User
+  , User = new UserModel('localhost', 27017);
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: 'your secret here' }));
+  app.use(express.compiler({ src: __dirname + '/public', enable: ['sass'] }));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+// Helpers for views -----
+
+app.dynamicHelpers({
+  session: function (req, res) {
+    return req.session;
+  },
+
+  flash: function (req, res) {
+    return req.flash();
+  }
+});
+
+// Socket.io -----
+
+var connections = {};
+var online_users = {}; // online user list for view
+
+// check empty object
+function is_empthy_obj(obj) {
+  for (var prop in obj)
+      return false;
+  return true;
+}
+
+// public chat room
+var public_room = io.of('/room').on('connection', function (socket) {
+  // add and share new user
+  socket.set('name', current_user.name);
+  socket.get('name', function (err, name) {
+    // check concurrence
+    if (!connections[name]) {
+      connections[name] = { sockets: {} };
+      online_users[name] = true;
+    }
+    connections[name].sockets[socket.id] = socket;
+
+    public_room.emit('online_users', online_users);
+    public_room.emit('msg', '<b>' + name + '</b> is joined');
+  });
+
+  socket.on('msg', function (msg) {
+    socket.get('name', function (err, name) {
+      public_room.emit('msg', '<b>' + name + '</b>: ' + msg);
+    });
+  });
+
+  socket.on('pmsg', function (name, msg) {
+  });
+
+  // remove and share disconnected user
+  socket.on('disconnect', function () {
+    socket.get('name', function (err, name) {
+      delete connections[name].sockets[socket.id];
+      if (is_empthy_obj(connections[name].sockets)) {
+        delete connections[name];
+        delete online_users[name];
+      }
+
+      public_room.emit('online_users', online_users);
+      public_room.emit('msg', '<b>' + name + '</b> is left');
+    });
+  });
+});
+
+// Routes -----
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+app.get('/join', function (req, res) {
+  res.render('user/join.jade', {
+    title: 'Join'
+  });
+});
+
+app.post('/join', function (req, res) {
+  if (req.param('password') == req.param('password_confirmation')) {
+    User.save({
+      name: req.param('name'),
+      password: req.param('password')
+    }, function (error, docs) {
+      req.session.user = user;
+      current_user = user;
+      req.flash('info', 'Register is successfully. Now you can login');
+      res.redirect('/');
+    });
+  }
+});
+
+app.get('/login', function(req, res) {
+  if (!req.session.user)
+    res.render('user/login.jade', {
+      title: 'Login'
+    });
+  else
+    res.redirect('/');
+});
+
+var current_user = false;
+
+app.post('/login', function (req, res) {
+  User.findByName(req.param('name'), function (error, user) {
+    if (!req.session.user && user.password == req.param('password')) {
+      req.session.user = user;
+      current_user = user;
+      req.flash('info', 'Successfully loged in');
+    } else {
+      req.flash.info('error', 'Login is failed');
+    }
+    res.redirect('/');
+  });
+});
+
+app.get('/user/:name?', function (req, res) {
+  if (req.params.name)
+    User.findByName(req.params.name, function (error, user) {
+      title = user.name;
+      if (req.session.user)
+        if (req.session.user.name == req.params.name)
+          title = 'My profile';
+      res.render('user/show.jade', {
+        title: title,
+        user: user
+      });
+    });
+  else
+    User.findAll( function (error, users) {
+      res.render('user/index.jade', {
+        title: 'Users',
+        users: users
+      });
+    });
+});
+
+app.get('/logout', function (req, res) {
+  req.session.user = null;
+  current_user = false;
+  res.redirect('/');
+});
+
+app.get('/room/:name?', function (req, res) {
+  if (req.params.name) {}
+  else
+    if (req.session.user) {
+      User.findAll( function (error, users) {
+        res.render('room.jade', {
+          title: 'Public chat room'
+        });
+      });
+    } else
+      res.redirect('/login');
+});
+
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
---
+++ user.js
@@ -0,0 +1,74 @@
+var Db          = require('mongodb').Db
+  , Connection  = require('mongodb').Connection
+  , Server      = require('mongodb').Server
+  , BSON        = require('mongodb').BSON
+  , ObjectID    = require('mongodb').ObjectID;
+
+User = function (host, port) {
+  this.db = new Db('exchat', new Server(host, port, {auto_reconnect: true}, {}));
+  this.db.open(function (){});
+}
+
+User.prototype.getCollection = function (callback) {
+  this.db.collection( 'user', function (error, user_collection) {
+    if (error) callback(error);
+    else callback(null, user_collection);
+  });
+}
+
+User.prototype.findAll = function (callback) {
+  this.getCollection( function (error, user_collection) {
+    if (error) callback(error);
+    else {
+      user_collection.find().toArray( function (error, results) {
+        if (error) callback(error);
+        else callback(null, results);
+      });
+    }
+  });
+}
+
+User.prototype.findByName = function (name, callback) {
+  this.getCollection( function (error, user_collection) {
+    if (error) callback(error);
+    else {
+      user_collection.findOne({name: name}, function (error, results) {
+        if (error) callback(error);
+        else callback(null, results);
+      });
+    }
+  });
+}
+
+User.prototype.findById = function (id, callback) {
+  this.getCollection( function (error, user_collection) {
+    if (error) callback(error);
+    else {
+      user_collection.findOne({_id: user_collection.db.bson_serializer.ObjectID.createFromHexString(id)}, function (error, results) {
+        if (error) callback(error);
+        else callback(null, results);
+      });
+    }
+  });
+}
+
+User.prototype.save = function (users, callback) {
+  this.getCollection( function (error, user_collection) {
+    if (error) callback(error);
+    else {
+      if (typeof(users.length) == "undefined")
+        users = [users];
+
+      for (var i = 0; i < users.length; i++) {
+        user = users[i];
+        user.created_at = new Date();
+      }
+    }
+
+    user_collection.insert(users, function () {
+      callback(null, users);
+    });
+  });
+}
+
+exports.User = User;
